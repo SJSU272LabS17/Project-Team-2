a312cb2e1a8114a8ddeecac0c5ec4ed016711754
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2017-05-05 01:26:42 -0700

    Update signup.php

--- project/php/signup.php
+++ project/php/signup.php
@@ -1,37 +1,42 @@
-
+<?php
+   session_start();
+ ?>
 <?php
 include "dbconnect.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = mysqli_real_escape_string($conn,$_POST['Username']);
-    $firstname = mysqli_real_escape_string($conn,$_POST['Firstname']);
-    $lastname = mysqli_real_escape_string($conn,$_POST['Lastname']);
+  //  $firstname = mysqli_real_escape_string($conn,$_POST['Firstname']);
+  //  $lastname = mysqli_real_escape_string($conn,$_POST['Lastname']);
     $password = mysqli_real_escape_string($conn,$_POST['Password']);
     $cpassword = mysqli_real_escape_string($conn,$_POST['Cpassword']);
-	  $mobile = mysqli_real_escape_string($conn,$_POST['Mobile']);
+	//  $mobile = mysqli_real_escape_string($conn,$_POST['Mobile']);
 	  $email = mysqli_real_escape_string($conn,$_POST['Email']);
 
 	if($password != $cpassword){
-		echo 'Your passwords do not match., <a href="login.php">Refill here</a>';
+		echo 'Your passwords do not match., <a href="../login.php">Refill here</a>';
 		exit();
 	}
-	$sql = mysqli_query($conn,"SELECT id FROM customer WHERE mobile='$mobile' LIMIT 1");
+	$sql = mysqli_query($conn,"SELECT customer_id FROM customer WHERE customer_email='$email' LIMIT 1");
 	$userMatch = mysqli_num_rows($sql);
     if ($userMatch > 0) {
-		echo 'Sorry your mobile number is already registered into the system, <a href="signup.php">click here</a>';
+		echo 'Sorry an account is already associated with this email,to Login, <a href="../login.php">click here</a>';
 		exit();
 	}
-	$sql = "INSERT INTO customer (customer_firstname, customer_lastname, customer_email	, customer_mobile, customer_login, customer_password)
-        VALUES('$firstname', '$lastname','$email','$mobile','$username','$password')" ;
+	$sql = "INSERT INTO customer (customer_name,  customer_email , customer_password)
+        VALUES('$username','$email','$password')" ;
   if(mysqli_query($conn, $sql)){
-    echo "Records added successfully.";
+    echo "Records added successfully dsadaasda.";
+    $_SESSION['logged']=true;
+    $_SESSION['username']=$email;
   }
   else{
+     $_SESSION['logged']=false;
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
   }
 
   // close connection
   mysqli_close($conn);
-  header("Location: index.php");
+  header("Location: ../index.php");
   exit();
 }
 ?>
