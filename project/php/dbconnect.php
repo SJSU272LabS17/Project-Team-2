5c023ffea9b452bb0a4a469ac54e355399cb474c
Shiva K Padma <shivapadma322@gmail.com>
2017-05-05 01:22:01 -0700

    made new db connection from cloud

--- project/php/dbconnect.php
+++ project/php/dbconnect.php
@@ -1,7 +1,8 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
+   // DB deployed in AWS cloud using RDS service.
+   define('DB_SERVER', 'grocerybargain.cavbzzgm6wkd.us-west-1.rds.amazonaws.com');
+   define('DB_USERNAME', 'master');
+   define('DB_PASSWORD', 'master123');
    define('DB_DATABASE', 'grocery_db');
    $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    if ($conn->connect_error) {
