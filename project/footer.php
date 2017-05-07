c919833db18d4c8577fe62ab530627d8d6be32f9
Shiva K Padma <shivapadma322@gmail.com>
2017-05-07 12:53:23 -0700

    Create footer.php

---
+++ project/footer.php
@@ -0,0 +1,10 @@
+<!-- footer -->
+
+	<div class="footer" >
+      <div class="container" style="position: relative;color:white" >
+       <p>&copy; CMPE 272 Team Project.</p>
+      </div>
+
+	</div>
+
+<!-- footer -->
