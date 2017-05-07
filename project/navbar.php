750395eee5954438e91546b196412e117bc93472
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2017-05-07 00:02:45 -0700

    Add files via upload

---
+++ project/navbar.php
@@ -0,0 +1,39 @@
+<!-- banner -->
+	<div class="banner">
+		<div class="w3l_banner_nav_left">
+			<nav class="navbar nav_bottom">
+			 <!-- Brand and toggle get grouped for better mobile display -->
+			  <div class="navbar-header nav_2">
+				  <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+			   </div>
+			   <!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+					<ul class="nav navbar-nav nav_1">
+						<li><a href="products.php">Products</a></li>
+						<li><a href="household.php">Fruits</a></li>
+            <li><a href="household.php">vegetables</a></li>
+						<li><a href="kitchen.php">Diary & Eggs</a></li>
+						<li><a href="short-codes.php">Meat & Fish</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Beverages<span class="caret"></span></a>
+							<div class="dropdown-menu mega-dropdown-menu w3ls_vegetables_menu">
+								<div class="w3ls_vegetables">
+									<ul>
+										<li><a href="drinks.php">Soft Drinks</a></li>
+										<li><a href="drinks.php">Juices</a></li>
+									</ul>
+								</div>
+							</div>
+						</li>
+						<li><a href="pet.php">Pet Food</a></li>
+            <li><a href="pet.php">Snacks</a></li>
+						<li><a href="bread.php">Bread & Bakery</a></li>
+					</ul>
+				 </div><!-- /.navbar-collapse -->
+			</nav>
+		</div>
