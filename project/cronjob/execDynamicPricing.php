2e12e956824e7b9614fbc5e98028a7e67a7aebf1
Shiva K Padma <shivapadma322@gmail.com>
2017-05-09 02:52:16 -0700

    php script to perform dynamic pricing

---
+++ project/cronjob/execDynamicPricing.php
@@ -0,0 +1,30 @@
+<?php
+   // DB deployed in AWS cloud using RDS service.
+   define('DB_SERVER', 'grocerybargain.cavbzzgm6wkd.us-west-1.rds.amazonaws.com');
+   define('DB_USERNAME', 'master');
+   define('DB_PASSWORD', 'master123');
+   define('DB_DATABASE', 'grocery_db');
+   $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $query1="update product set product_discount = product_discount + 15 
+where product_sell_by_date = curdate()+1 
+and dynamic_pricing_flag = 'Y'
+and product_discount <= 75";
+  $query2="update product set product_discount = product_discount + 5 
+where product_sell_by_date between curdate()+2 and curdate()+4
+and dynamic_pricing_flag = 'Y'
+and product_discount<=70";
+  $query3 ="update product set product_discount = product_discount + 2 
+where product_sell_by_date between curdate()+5 and curdate()+8
+and dynamic_pricing_flag = 'Y'
+and product_discount<=60";
+   if ($conn->connect_error) {
+   echo "Connection Failed";
+   die("Connection failed: " . $conn->connect_error);
+  }
+  else{
+    echo "Connected successfully and Executing Queries on Dynamic Pricing.";
+    $conn->query($query1);
+    $conn->query($query2);
+    $conn->query($query3);
+  }
+?>
