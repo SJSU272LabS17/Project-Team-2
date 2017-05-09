29cbc0e1aa9e02ac32e857e358686771c19f7863
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2017-05-09 01:38:38 -0700

    Update and rename vendorinventory.html to vendorinventory.php
    changes for adding the header menu

--- project/vendorinventory.php
+++ project/vendorinventory.php
@@ -3,15 +3,24 @@
 <head>
   <title>Vendor Inventory</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta name="keywords" content="vendor details of inventory" />
+  <!-- //for-mobile-apps -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+  <!-- font-awesome icons -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+  <!-- //font-awesome icons -->
+  <!-- js -->
+  <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+  <!-- //js -->
+  <link href='//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+  <!-- start-smoth-scrolling -->
 </head>
 <body>
-
-
-
+  <?php
+  	include_once("vendorheader.php");
+  ?>
 <div class="container">
   <h2>Vendory Inventory</h2>
   <p>Please click on each of the product below and tell us the inventory you have</p>
@@ -178,6 +187,38 @@
 
   </div>
 </div>
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');
+        }
+    );
+});
+
+</script>
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear'
+				};
+			*/
+
+			$().UItoTop({ easingType: 'easeOutQuart' });
+
+			});
+	</script>
+<!-- //here ends scrolling icon -->
 
 </body>
 </html>
