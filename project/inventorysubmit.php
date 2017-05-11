817d0efaedd24e6fe0b520f39e335df0254c3543
Shiva K Padma <shivapadma322@gmail.com>
2017-05-11 05:43:14 -0700

    adding products from inventorypage

--- project/inventorysubmit.php
+++ project/inventorysubmit.php
@@ -6,39 +6,31 @@
 include_once("php/dbconnect.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $sellername = $_POST["sellername"];
-  $password = $_POST["password"];
-  $email = $_POST["email"];
-  $address = $_POST["address"];
-  $city = $_POST["city"];
-  $zipcode= $_POST["zipcode"];
-  $state = $_POST["state"];
-  $phonenumber = $_POST["phonenumber"];
+  $quantity = $_POST["quantity"];
+  $price = $_POST["perunit"];
+  $quality = $_POST["quality"];
+  echo $quality;
+  $expiry_date = date('Y-m-d', strtotime(' +'.$quality.' days'));
+  $discountpercentage = $_POST["discountpercentage"];
+  $dp_flag= $_POST["dp_flag"];
+  $seller_id = $_SESSION['seller_id'];
 }
 
+$sql = "INSERT INTO product (product_available_units, product_price, product_sell_by_date, product_discount, dynamic_pricing_flag, seller_id)
+VALUES ('$quantity', '$price', '$expiry_date', '$discountpercentage', '$dp_flag', '$seller_id')";
 
-$sql = mysqli_query($conn,"SELECT seller_id FROM seller WHERE seller_email='$email' LIMIT 1");
-$userMatch = mysqli_num_rows($sql);
-  if ($userMatch > 0) {
-  echo 'Sorry an account is already associated with this email,to Login, <a href="vendorlogin.php">click here</a>';
-  exit();
+if ($conn->query($sql) == TRUE)
+{
+  echo "Success: " . $sql . "<br>";
+
 }
-
-$sql = "INSERT INTO seller (seller_name, seller_email, seller_address, seller_city, seller_zipcode, seller_state,  seller_password, seller_phonenumber)
-VALUES ('$sellername', '$email', '$address', '$city', '$zipcode', '$state', '$password', '$phonenumber')";
-
-if ($conn->query($sql) === TRUE) {
-  $_SESSION['logged']=true;
-  $_SESSION['sellername']=$username;
-  $_SESSION['email'] = $email;
-   header('Location: vendorinventory.php');
-
-} else {
-    $_SESSION['logged']=false;
+else {
+    
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$conn->close();
+$conn.close();
+
 ?>
 </body>
 </html>
