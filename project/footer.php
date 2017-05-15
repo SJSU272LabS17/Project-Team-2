842786b6093d1b3f32a865f4f46d54abe178e1b7
Shiva K Padma <shivapadma322@gmail.com>
2017-05-14 17:58:28 -0700

    Update footer.php

--- project/footer.php
+++ project/footer.php
@@ -1,10 +1,9 @@
 <!-- footer -->
 
 	<div class="footer" >
-      <div class="container" style="position: relative;color:white; background-color:#212121; text-align:center;""  >
-       <p>&copy; CMPE 272 Team Project.</p>
+      <div class="container" style="position: relative;color:white; background-color:#212121; text-align:center;"  >
+       <p>&copy; CMPE 272 Team Project </p>
       </div>
-
 	</div>
 
 <!-- footer -->
