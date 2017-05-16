c2c0fbb4d8f57797531484d0c61df26b80461ee6
Shiva K Padma <shivapadma322@gmail.com>
2017-05-15 17:43:34 -0700

    Update index.php

--- project/index.php
+++ project/index.php
@@ -51,7 +51,7 @@
 					<ul class="slides">
 						<li>
 							<div class="w3l_banner_nav_right_banner">
-								<h3>Make your <span>food</span> with Spicy.</h3>
+								<h3> <span></span> </h3>
 								<div class="more">
 									<a href="products.php" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
 								</div>
@@ -59,7 +59,7 @@
 						</li>
 						<li>
 							<div class="w3l_banner_nav_right_banner1">
-								<h3>Make your <span>food</span> with Spicy.</h3>
+								<h3><span>food</span> </h3>
 								<div class="more">
 									<a href="products.php" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
 								</div>
@@ -67,7 +67,7 @@
 						</li>
 						<li>
 							<div class="w3l_banner_nav_right_banner2">
-								<h3>upto <i>50%</i> off.</h3>
+								<h3> <i></i></h3>
 								<div class="more">
 									<a href="products.php" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
 								</div>
